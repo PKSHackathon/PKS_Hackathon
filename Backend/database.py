8ba7629cbb66022bc666c727ff9e02f85fab654f
komputer\Leszczyński <danielopyd99@gmail.com>
2017-10-28 14:24:01 +0200

    ok

--- Backend/database.py
+++ Backend/database.py
@@ -1,13 +1,13 @@
 import openpyxl
-z = 0
-wb = openpyxl.load_workbook('baza.xlsx')
-wb.get_sheet_names()
-event = wb.get_sheet_by_name('Arkusz1')
-for i in range(4,54,1):
-    for z in range(2, 7, 1):
-        if event.cell(row=i, column=z).value:
+events= [None]
+y = 0
+wb = openpyxl.load_workbook('baza.xlsx')                    # Załadowanie bazy
+wb.get_sheet_names()                                        # Pobranie nazw arkuszy
+event = wb.get_sheet_by_name('Arkusz1')                     # Przypisanie arkusza do zmiennej event
+for i in range(4, 54, 1):                                   # Przejście przez wiersze
+    for z in range(2, 7, 1):                                # Przejście przez komórki
+        if event.cell(row=i, column=z).value:               # Sprawdzanie czy komórka zawiera dane
             print(event.cell(row=i, column=z).value)
-
     else:
-        z += 1
-
+        y += 1
+    print("=============")
