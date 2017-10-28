ed80ff61405f87225dc4b97f57bb6b6f8e241b1e
LAPTOP-KQMV90GM\admin <wojokarolg9@gmail.com>
2017-10-28 19:22:16 +0100

    strona

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -11,7 +11,8 @@ function listenButtons(){
 	{
 		buttons[x].addEventListener('click',showActiv,false);		//Pokazywanie dni pod przyciskiem
 	}
-
+	let send = document.querySelector('#send');
+	send.addEventListener('click',sendData,false);
 
 }
 
@@ -20,4 +21,25 @@ function showActiv(){
 let dayNr = this.value;
 let list = document.querySelector(".day-" + dayNr);
 list.classList.toggle('hidden');
+}
+
+function sendData(){
+let title = document.querySelector('#title').value;
+let place = document.querySelector('#place').value;
+let day = document.querySelector('#day').value;
+let month = document.querySelector('#month').value;
+let year = document.querySelector('#year').value;
+let hour = document.querySelector('#hours').value;
+let min = document.querySelector('#min').value;
+let link = document.querySelector('#link').value;
+let tags = document.querySelector('#tags').value;
+let description = document.querySelector('#description');
+let add = document.querySelector('#add').value;
+
+
+
+
+
+
+
 }
