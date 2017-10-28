9f9c1aee4c9f7a52d50c9016b9a5a89474992fda
komputer\Leszczyński <danielopyd99@gmail.com>
2017-10-28 12:59:09 +0200

    fix

--- Backend/database.py
+++ Backend/database.py
@@ -1,4 +1,5 @@
 import openpyxl
+i = 0
 wb = openpyxl.load_workbook('baza.xlsx')
 wb.get_sheet_names()
 sheet = wb.get_sheet_by_name('Arkusz1')
@@ -6,4 +7,5 @@ for i in range(4,53,1):
     if sheet.cell(row=i, column=2).value != None:
         print(i, sheet.cell(row=i, column=2).value)
     else:
-        print(i, sheet.cell(row=i+1, column=2).value)
+        i +=1
+
