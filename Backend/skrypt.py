f11a58e5df2396593a6206ae837f63fa8d513353
komputer\Leszczyński <danielopyd99@gmail.com>
2017-10-28 15:08:48 +0200

    skrypt v.2

--- Backend/skrypt.py
+++ Backend/skrypt.py
@@ -1,8 +1,11 @@
 import openpyxl
 wb = openpyxl.load_workbook('baza.xlsx')
-wb.get_sheet_names()
 event = wb.get_sheet_by_name('Arkusz1')
 for i in range(4, 54, 1):
-    for z in range(2, 7, 1):
-        a = (nazwa="event.cell(row=i, column=2).value",data="event.cell(row=i, column=3).value",godzina="event.cell(row=i, column=3).value", miejsca="event.cell(row=i, column=4).value",uwagi="event.cell(row=i, column=5).value")
-        a.save()
+    a = event()
+    a.nazwa = event.cell(row=i, column=2).value
+    a.data = event.cell(row=i, column=3).value
+    a.godzina = event.cell(row=i, column=4).value
+    a.miejsce = event.cell(row=i, column=5).value
+    a.uwagi = event.cell(row=i, column=6).value
+a.save()
