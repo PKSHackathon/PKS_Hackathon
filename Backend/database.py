83d9c502e0c114282ec0659987fa1885b14d7ce9
komputer\Leszczyński <danielopyd99@gmail.com>
2017-10-28 12:49:38 +0200

    Aktualizacja pliku bazy.

--- Backend/database.py
+++ Backend/database.py
@@ -1,1 +1,9 @@
 import openpyxl
+wb = openpyxl.load_workbook('baza.xlsx')
+wb.get_sheet_names()
+sheet = wb.get_sheet_by_name('Arkusz1')
+for i in range(4,53,1):
+    if sheet.cell(row=i, column=2).value != None:
+        print(i, sheet.cell(row=i, column=2).value)
+    else:
+        print("")
